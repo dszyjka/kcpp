5bab0010646fe7b675c13b0c6c8402b8167dfaf5
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-06-16 14:50:53 +0200

    feat: add done 5.1 to ZadClassKcpp

--- kcppZadania/ZadClassKcpp/include/sek5ZadClass.h
+++ kcppZadania/ZadClassKcpp/include/sek5ZadClass.h
@@ -1,13 +1,63 @@
 #ifndef SEK5ZADCLASS_H
-#define SEK5ZADLCASS_H
+#define SEK5ZADCLASS_H
 
 #include "ZadKcpp.h"
+#include <iostream>
+#include <string>
+
+class Konto {
+    public:
+        std::string wlasciciel;
+
+        Konto();
+        Konto(const std::string& imie, double kwota);
+
+        void Wplac(double kwota) {
+            if (kwota > 0) {
+            saldo += kwota;
+            }
+        }
+
+        void wyplac(double kwota) {
+            if (kwota >= 0 && kwota <= saldo) {
+                saldo -= kwota;
+            }
+        }
+
+        inline double PobierzSaldo() const {
+            return saldo;
+        }
+
+        void Wypisz() const;
+        void ZmienNumer(int nowyNumer);
+        
+    protected:
+        std::string typKonta;
+
+        void UstawTyp(const std::string& typ) {
+            typKonta = typ;
+        }
+
+    private:
+        double saldo;
+        int numer;
+
+        bool PoprawnyNumer(int wartosc) const {
+        return wartosc > 0;
+        }
+};
+
+class KontoFirmowe : public Konto {
+    public:
+        void UstawJakoFirmowe() {
+        UstawTyp("firmowe");
+    }
+};
 
 class Sek5ZadClass: public ZadKcpp {
     public:
         Sek5ZadClass();
         void start() override;
-        
 };
 
 #endif
---
+++ kcppZadania/ZadClassKcpp/src/sek5ZadClass.cc
@@ -0,0 +1,41 @@
+#include "../include/sek5ZadClass.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Konto::Konto() : wlasciciel("brak"), typKonta("osobiste"), saldo(0.0), numer(1) {}
+
+Konto::Konto(const string& imie, double kwota)
+    : wlasciciel(imie), typKonta("oszczednosciowe"), saldo(kwota), numer(100) {}
+
+void Konto::Wypisz() const {
+    std::cout << "Wlasciciel: " << wlasciciel << std::endl;
+    std::cout << "Typ konta: " << typKonta << std::endl;
+    std::cout << "Numer: " << numer << std::endl;
+    std::cout << "Saldo: " << saldo << std::endl;
+}
+
+void Konto::ZmienNumer(int nowyNumer) {
+    if (PoprawnyNumer(nowyNumer)) {
+        numer = nowyNumer;
+    }
+}
+
+Sek5ZadClass::Sek5ZadClass(): ZadKcpp("Zad Class", "5") {}
+void Sek5ZadClass::start() {
+    Konto konto("Jan", 250.0);
+    konto.Wplac(50.0);
+    konto.Wypisz();
+    konto.Wplac(20.0);
+    konto.ZmienNumer(1234);
+    konto.Wypisz();
+
+    cout << "Saldo odczytane funkcja inline: " << konto.PobierzSaldo() << endl;
+
+    KontoFirmowe firma;
+    firma.wlasciciel = "Januszex sp. z.o.o";
+    firma.UstawJakoFirmowe();
+    firma.Wplac(1000.0);
+    firma.Wypisz();
+}
